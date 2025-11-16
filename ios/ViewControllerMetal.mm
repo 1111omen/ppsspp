6c84cfd672893306ae6d30af2f6445bb2748dd95
Henrik Rydgård <hrydgard@gmail.com>
2025-11-16 17:19:54 +0100

    Move the iOS vulkan context to its own file

--- ios/ViewControllerMetal.mm
+++ ios/ViewControllerMetal.mm
@@ -3,20 +3,14 @@
 #import "DisplayManager.h"
 #import "iOSCoreAudio.h"
 
-#include "Common/Log.h"
+#include "ios/iOSVulkanContext.h"
 
-#include "Common/GPU/Vulkan/VulkanLoader.h"
-#include "Common/GPU/Vulkan/VulkanContext.h"
-#include "Common/GPU/Vulkan/VulkanRenderManager.h"
-#include "Common/GPU/thin3d.h"
-#include "Common/GPU/thin3d_create.h"
-#include "Common/Data/Text/Parsers.h"
+#include "Common/Log.h"
 #include "Common/System/Display.h"
 #include "Common/System/System.h"
 #include "Common/System/OSD.h"
 #include "Common/System/NativeApp.h"
 #include "Common/System/Request.h"
-#include "Common/GraphicsContext.h"
 #include "Common/Thread/ThreadUtil.h"
 #include "Common/TimeUtil.h"
 
@@ -25,149 +19,6 @@
 #include "Core/System.h"
 
 #include "GPU/Vulkan/VulkanUtil.h"
-
-// ViewController lifecycle:
-// https://www.progressconcepts.com/blog/ios-appdelegate-viewcontroller-method-order/
-
-enum class GraphicsContextState {
-	PENDING,
-	INITIALIZED,
-	FAILED_INIT,
-	SHUTDOWN,
-};
-
-class IOSVulkanContext : public GraphicsContext {
-public:
-	IOSVulkanContext() {}
-	~IOSVulkanContext() {
-		delete g_Vulkan;
-		g_Vulkan = nullptr;
-	}
-
-	bool InitAPI();
-
-	bool InitFromRenderThread(CAMetalLayer *layer, int desiredBackbufferSizeX, int desiredBackbufferSizeY);
-	void ShutdownFromRenderThread() override;  // Inverses InitFromRenderThread.
-
-	void Shutdown() override;
-	void Resize() override {}
-
-	void *GetAPIContext() override { return g_Vulkan; }
-	Draw::DrawContext *GetDrawContext() override { return draw_; }
-
-private:
-	VulkanContext *g_Vulkan = nullptr;
-	Draw::DrawContext *draw_ = nullptr;
-	GraphicsContextState state_ = GraphicsContextState::PENDING;
-};
-
-bool IOSVulkanContext::InitFromRenderThread(CAMetalLayer *layer, int desiredBackbufferSizeX, int desiredBackbufferSizeY) {
-	INFO_LOG(Log::G3D, "IOSVulkanContext::InitFromRenderThread: desiredwidth=%d desiredheight=%d", desiredBackbufferSizeX, desiredBackbufferSizeY);
-	if (!g_Vulkan) {
-		ERROR_LOG(Log::G3D, "IOSVulkanContext::InitFromRenderThread: No Vulkan context");
-		return false;
-	}
-
-	VkResult res = g_Vulkan->InitSurface(WINDOWSYSTEM_METAL_EXT, (__bridge void *)layer, nullptr);
-	if (res != VK_SUCCESS) {
-		ERROR_LOG(Log::G3D, "g_Vulkan->InitSurface failed: '%s'", VulkanResultToString(res));
-		return false;
-	}
-
-	bool useMultiThreading = g_Config.bRenderMultiThreading;
-	if (g_Config.iInflightFrames == 1) {
-		useMultiThreading = false;
-	}
-
-	draw_ = Draw::T3DCreateVulkanContext(g_Vulkan, useMultiThreading);
-
-	VkPresentModeKHR presentMode = ConfigPresentModeToVulkan(draw_);
-
-	// This MUST run on the main thread. We're taking our chances with a dispatch_sync here.
-	g_Vulkan->InitSwapchain(presentMode);
-
-	if (false) {
-		delete draw_;
-		ERROR_LOG(Log::G3D, "InitSwapchain failed");
-		g_Vulkan->DestroySwapchain();
-		g_Vulkan->DestroySurface();
-		g_Vulkan->DestroyDevice();
-		g_Vulkan->DestroyInstance();
-		return false;
-	}
-
-	SetGPUBackend(GPUBackend::VULKAN);
-	bool shaderSuccess = draw_->CreatePresets();  // Doesn't fail, we ship the compiler.
-	_assert_msg_(shaderSuccess, "Failed to compile preset shaders");
-	draw_->HandleEvent(Draw::Event::GOT_BACKBUFFER, g_Vulkan->GetBackbufferWidth(), g_Vulkan->GetBackbufferHeight());
-
-	VulkanRenderManager *renderManager = (VulkanRenderManager *)draw_->GetNativeObject(Draw::NativeObject::RENDER_MANAGER);
-	renderManager->SetInflightFrames(g_Config.iInflightFrames);
-	return true;
-}
-
-void IOSVulkanContext::ShutdownFromRenderThread() {
-	INFO_LOG(Log::G3D, "IOSVulkanContext::Shutdown");
-	draw_->HandleEvent(Draw::Event::LOST_BACKBUFFER, g_Vulkan->GetBackbufferWidth(), g_Vulkan->GetBackbufferHeight());
-	delete draw_;
-	draw_ = nullptr;
-	g_Vulkan->WaitUntilQueueIdle();
-	g_Vulkan->PerformPendingDeletes();
-	g_Vulkan->DestroySwapchain();
-	g_Vulkan->DestroySurface();
-	INFO_LOG(Log::G3D, "Done with ShutdownFromRenderThread");
-}
-
-void IOSVulkanContext::Shutdown() {
-	INFO_LOG(Log::G3D, "Calling NativeShutdownGraphics");
-	g_Vulkan->DestroyDevice();
-	g_Vulkan->DestroyInstance();
-	// We keep the g_Vulkan context around to avoid invalidating a ton of pointers around the app.
-	finalize_glslang();
-	INFO_LOG(Log::G3D, "IOSVulkanContext::Shutdown completed");
-}
-
-bool IOSVulkanContext::InitAPI() {
-	INFO_LOG(Log::G3D, "IOSVulkanContext::Init");
-	init_glslang();
-
-	g_LogOptions.breakOnError = true;
-	g_LogOptions.breakOnWarning = true;
-	g_LogOptions.msgBoxOnError = false;
-
-	INFO_LOG(Log::G3D, "Creating Vulkan context");
-	Version gitVer(PPSSPP_GIT_VERSION);
-
-	std::string errorStr;
-	if (!VulkanLoad(&errorStr)) {
-		ERROR_LOG(Log::G3D, "Failed to load Vulkan driver library: %s", errorStr.c_str());
-		state_ = GraphicsContextState::FAILED_INIT;
-		return false;
-	}
-
-	if (!g_Vulkan) {
-		// TODO: Assert if g_Vulkan already exists here?
-		g_Vulkan = new VulkanContext();
-	}
-
-	VulkanContext::CreateInfo info{};
-	InitVulkanCreateInfoFromConfig(&info);
-	if (!g_Vulkan->CreateInstanceAndDevice(info)) {
-		delete g_Vulkan;
-		g_Vulkan = nullptr;
-		state_ = GraphicsContextState::FAILED_INIT;
-		return false;
-	}
-
-	g_Vulkan->SetCbGetDrawSize([]() {
-		return VkExtent2D {(uint32_t)g_display.pixel_xres, (uint32_t)g_display.pixel_yres};
-	});
-
-	INFO_LOG(Log::G3D, "Vulkan device created!");
-	state_ = GraphicsContextState::INITIALIZED;
-	return true;
-}
-
 
 #pragma mark -
 #pragma mark PPSSPPViewControllerMetal
